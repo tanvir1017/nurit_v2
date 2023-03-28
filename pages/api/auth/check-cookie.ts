656377d89dd13fe3f1c987a7c4059b1dc9affe78
Rakib544 <md.rakib10122003@gmail.com>
2023-03-28 19:40:06 +0600

    trying to find cookies error

--- pages/api/auth/check-cookie.ts
+++ pages/api/auth/check-cookie.ts
@@ -2,20 +2,27 @@ import { getCookie, hasCookie } from "cookies-next";
 import jwt from "jsonwebtoken";
 import { NextApiRequest, NextApiResponse } from "next";
 const login = async (req: NextApiRequest, res: NextApiResponse) => {
-  const has = hasCookie("u-auth", { req, res });
-  if (has) {
-    const getCookieFromBrowser = getCookie("u-auth", {
-      req,
-      res,
-    });
-    const verifiedToken = jwt.verify(
-      getCookieFromBrowser as string,
-      process.env.ACCESS_token as string
-    );
+  try {
+    const has = hasCookie("u-auth", { req, res });
+    if (has) {
+      const getCookieFromBrowser = getCookie("u-auth", {
+        req,
+        res,
+      });
+      const verifiedToken = jwt.verify(
+        getCookieFromBrowser as string,
+        process.env.ACCESS_token as string
+      );
 
-    res.status(200).json({ verifiedToken });
-  } else {
-    res.status(200).json({ token: "NOT FOUND" });
+      console.log(verifiedToken);
+
+      res.status(200).json({ verifiedToken });
+    } else {
+      res.status(200).json({ token: "NOT FOUND" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
   }
 };
 export default login;
