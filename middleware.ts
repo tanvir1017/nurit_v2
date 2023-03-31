a2feffc99fac8994064f5e146a875c8ed3eaa8ad
tanvir1017 <tanvir.hossaindev1@gmail.com>
2023-03-31 21:32:56 +0600

    fix: 🐛 Login redirect bug fixed
    Login redirect bug fixed

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,10 @@
 import type { NextRequest } from "next/server";
-import { NextResponse } from "next/server";
 
 export default function middleware(req: NextRequest) {
   const verify = req.cookies.get("u-auth");
-  if (verify && req.nextUrl.pathname.startsWith("/auth")) {
-    return NextResponse.rewrite(new URL("/404", req.url));
-  }
+  // if (verify && req.nextUrl.pathname.startsWith("/auth")) {
+  //   return NextResponse.rewrite(new URL("/404", req.url));
+  // }
   // if (process.env.NODE_ENV === "production") {
   //   if (
   //     verify &&
