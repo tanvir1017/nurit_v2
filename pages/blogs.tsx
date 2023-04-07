99d38f61c1a772c837f1e913e31f14411c3f01f3
tanvir1017 <tanvir.hossaindev1@gmail.com>
2023-04-07 15:29:18 +0600

    fix: 🐛 Blogs page [blogs] doesn't exist on returnBlogData as it
    Blogs page [blogs] doesn't exist on returnBlogData as it undefined
    
    BREAKING CHANGE: 🧨 Blogs page [blogs] doesn't exist on returnBlogData as it undefined

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,3 +1,4 @@
+import Skeleton from "@/components/shared/skeleton";
 import Metadata from "@/util/SEO/metadata";
 import swr from "swr";
 
@@ -6,37 +7,56 @@ const fetcher = (url: RequestInfo | URL) =>
 
 const Blogs = () => {
   const { data, isLoading, error } = swr("/api/blogs", fetcher);
-  const {
-    returnBlogData: { blogs },
-  } = data;
-
-  return (
-    <>
-      <Metadata
-        title="NurIT-Blogs"
-        name="blog page"
-        content="NurIT blogs"
-        // key="skill course, course, ms office, office 364"
-      />
+  let content = null;
+  if (!error && !data && isLoading) {
+    content = (
+      <div
+        id="card"
+        className="grid grid-cols-4 gap-5 place-items-center mt-14"
+      >
+        {[...Array(8).keys()].map((_, i) => (
+          <Skeleton key={i} />
+        ))}
+      </div>
+    );
+  }
+  if (!data && !isLoading && error) {
+    content = <p>Something went wrong!</p>;
+  }
+  if (!isLoading && !error && data) {
+    const {
+      returnBlogData: { blogs },
+    } = data;
+    content = (
       <main className="App">
-        <section className="container">
-          <div>
-            <h1 className="text-green-500">
-              {" "}
-              total blogs are available: {blogs.length}
-            </h1>
-            {blogs.map((item: any) => {
-              return (
-                <div key={item?.id}>
-                  <h1>{item.title}</h1>
-                </div>
-              );
-            })}
-          </div>
-        </section>
+        <Metadata
+          title="NurIT-Blogs"
+          name="blog page"
+          content="NurIT blogs"
+          // key="skill course, course, ms office, office 364"
+        />
+        <main className="App">
+          <section className="container">
+            <div>
+              <h1 className="text-green-500">
+                {" "}
+                total blogs are available: {blogs.length}
+              </h1>
+              {blogs.map((item: any) => {
+                return (
+                  <div key={item?.id}>
+                    <h1>{item.title}</h1>
+                  </div>
+                );
+              })}
+            </div>
+          </section>
+        </main>
       </main>
-    </>
-  );
+    );
+  }
+
+  return content;
 };
 
 export default Blogs;
