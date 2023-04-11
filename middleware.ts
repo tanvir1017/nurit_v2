f68f901479e9a6c870233f1f1da31cec0a3f0944
tanvir1017 <tanvir.hossaindev1@gmail.com>
2023-04-11 10:07:58 +0600

    feat: 🎸 Secrure route by the edge runtime package and middlewar
    Secrure route by the edge runtime package and middlewar

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,34 @@
-import type { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { verify } from "./util/jwt";
 
-export default function middleware(req: NextRequest) {
-  const verify = req.cookies.get("u-auth");
-  // if (verify && req.nextUrl.pathname.startsWith("/auth")) {
-  //   return NextResponse.rewrite(new URL("/404", req.url));
-  // }
-  // if (process.env.NODE_ENV === "production") {
-  //   if (
-  //     verify &&
-  //     req.nextUrl.pathname.startsWith("/api") &&
-  //     req.nextUrl.pathname.includes("/api")
-  //   ) {
-  //     return NextResponse.rewrite(new URL("/404", req.url));
-  //   }
-  // }
+const secret = process.env.ACCESS_TOKEN || "secret";
+
+export default async function middleware(req: NextRequest) {
+  const jwt = req.cookies.get("u-auth");
+  const jwtValue = jwt?.value;
+  const { pathname } = req.nextUrl;
+
+  const PUBLIC_FILE = /\.(.*)$/;
+  if (PUBLIC_FILE.test(pathname)) return NextResponse.next();
+
+  if (pathname.startsWith("/dashboard")) {
+    if (jwt === undefined) {
+      req.nextUrl.pathname = "/auth/login";
+      return NextResponse.redirect(req.nextUrl);
+    }
+
+    try {
+      const decode = await verify(jwtValue as string, secret);
+      if (decode?.role !== "ADMIN") {
+        req.nextUrl.pathname = "/404";
+        return NextResponse.redirect(req.nextUrl);
+      }
+      return NextResponse.next();
+    } catch (error) {
+      req.nextUrl.pathname = "/auth/login";
+      return NextResponse.redirect(req.nextUrl);
+    }
+  }
+
+  return NextResponse.next();
 }
