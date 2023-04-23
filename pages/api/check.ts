62da315d25a796e2ad3bee36f6157b891b675046
tanvir1017 <tanvir.hossaindev1@gmail.com>
2023-04-23 13:28:47 +0600

    [500] internal server error

--- pages/api/check.ts
+++ pages/api/check.ts
@@ -1,7 +1,9 @@
+import { getAllUser } from "@/lib/dbOperatons/users.prisma";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function Slug(req: NextApiRequest, res: NextApiResponse) {
+  const user = await getAllUser();
   return res.status(200).send({
-    check: "hello",
+    user,
   });
 }
