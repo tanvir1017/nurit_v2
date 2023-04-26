01f779a9a0feb05a248ba0eb9aad52788c991030
tanvir1017 <tanvir.hossaindev1@gmail.com>
2023-04-26 10:24:54 +0600

    feat: 🎸 Blog page has typography plugin from tailwindcss
    Blog page has typography plugin from tailwindcss
    
    BREAKING CHANGE: 🧨 Blog page has typography plugin from tailwindcss
    
    ✅ Closes: Blog page has typography plugin from tailwindcss

--- pages/blogs/blog.tsx
+++ pages/blogs/blog.tsx
@@ -1,27 +1,12 @@
 import Link from "next/link";
 
 const Blog = ({ blog }: any) => {
-  const {
-    cover,
-    createdAt,
-    html,
-    id,
-    readTime,
-    slug,
-    sub_title,
-    tags,
-    thumbnail,
-    title,
-    updatedAt,
-    views,
-  } = blog;
-
   return (
     <>
-      <Link href={`/blog/${slug}`}>
-        {title}
+      <Link href={`/blog/${blog?.slug}`}>
+        {blog?.title}
         <div></div>
-        {readTime}
+        {blog?.readTime}
       </Link>
     </>
   );
